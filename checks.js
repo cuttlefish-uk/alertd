8fc4506e1547337182c1c9e70c6a60d81e442879
Jude Venn <judev@cuttlefish.com>
2012-08-02 10:17:52 +0100

    check values can now be functions, evaluated each check

--- checks.js
+++ checks.js
@@ -1,6 +1,13 @@
 
+var valueof = function(v) {
+  if (typeof v === 'function') {
+    return v();
+  }
+  return v;
+};
+
 exports.http = function(res, on_error) {
-  var statusCode = this.config.statusCode === undefined ? 200 : this.config.statusCode;
+  var statusCode = this.config.statusCode === undefined ? 200 : valueof(this.config.statusCode);
   var duration_secs = res.duration / 1000;
   if (res.statusCode !== statusCode) {
     return on_error('critical', this.name + ' response: ' + res.statusCode + ' (expected ' + statusCode + ')\n' + duration_secs + 's');
@@ -10,8 +17,8 @@ exports.http = function(res, on_error) {
       return on_error('critical', this.name + ' body doesn\'t match ' + this.config.bodyMatch + '\n' + duration_secs + 's');
     }
   }
-  if (this.config.duration !== undefined && duration_secs >= this.config.duration) {
-    return on_error('warning', this.name + ' took ' + duration_secs + 's (warning at ' + this.config.duration + 's)');
+  if (this.config.duration !== undefined && duration_secs >= valueof(this.config.duration)) {
+    return on_error('warning', this.name + ' took ' + duration_secs + 's (warning at ' + valueof(this.config.duration) + 's)');
   }
   on_error('ok', this.name + ' ok (' + duration_secs + 's)');
 };
@@ -28,13 +35,13 @@ exports.http_200 = function(res, on_error) {
 
 exports.value_over = function(res, on_error) {
   if (typeof this.config.critical !== undefined) {
-    if (res > this.config.critical) {
-      return on_error('critical', this.name + ' is ' + res + ' (has exceeded ' + this.config.critical + ')');
+    if (res > valueof(this.config.critical)) {
+      return on_error('critical', this.name + ' is ' + res + ' (has exceeded ' + valueof(this.config.critical) + ')');
     }
   }
   if (typeof this.config.warning !== undefined) {
-    if (res > this.config.warning) {
-      return on_error('warning', this.name + ' is ' + res + ' (has exceeded ' + this.config.warning + ')');
+    if (res > valueof(this.config.warning)) {
+      return on_error('warning', this.name + ' is ' + res + ' (has exceeded ' + valueof(this.config.warning) + ')');
     }
   }
   on_error('ok', this.name + ' ok');
@@ -43,13 +50,13 @@ exports.value_gt = exports.value_over;
 
 exports.value_under = function(res, on_error) {
   if (typeof this.config.critical !== undefined) {
-    if (res < this.config.critical) {
-      return on_error('critical', this.name + ' is ' + res + ' (has fallen below ' + this.config.critical + ')');
+    if (res < valueof(this.config.critical)) {
+      return on_error('critical', this.name + ' is ' + res + ' (has fallen below ' + valueof(this.config.critical) + ')');
     }
   }
   if (typeof this.config.warning !== undefined) {
-    if (res < this.config.warning) {
-      return on_error('warning', this.name + ' is ' + res + ' (has fallen below ' + this.config.warning + ')');
+    if (res < valueof(this.config.warning)) {
+      return on_error('warning', this.name + ' is ' + res + ' (has fallen below ' + valueof(this.config.warning) + ')');
     }
   }
   on_error('ok', this.name + ' ok');
@@ -58,13 +65,13 @@ exports.value_lt = exports.value_under;
 
 exports.value_equal = function(res, on_error) {
   if (typeof this.config.critical !== undefined) {
-    if (res == this.config.critical) {
-      return on_error('critical', this.name + ' is ' + this.config.critical);
+    if (res == valueof(this.config.critical)) {
+      return on_error('critical', this.name + ' is ' + valueof(this.config.critical));
     }
   }
   if (typeof this.config.warning !== undefined) {
-    if (res == this.config.warning) {
-      return on_error('warning', this.name + ' is ' + this.config.warning);
+    if (res == valueof(this.config.warning)) {
+      return on_error('warning', this.name + ' is ' + valueof(this.config.warning));
     }
   }
   on_error('ok', this.name + ' ok');
